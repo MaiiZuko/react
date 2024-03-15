0a0f23a12a10cc4a5d5028553f852ae5cc0b9681
MaiiZuko <maiaramzucco@gmail.com>
2024-03-15 10:28:07 -0300

    update

--- organo/src/componentes/formulario/index.js
+++ organo/src/componentes/formulario/index.js
@@ -1,7 +1,18 @@
 import './Formulario.css'
 import CampoTexto from '../campoTexto/index'
+import ListaSuspensa from '../listaSuspensa'
 
 const Formulario = () => {
+
+    const times = {
+        'Suporte': 'Suporte',
+        'Mid': 'Mid',
+        'Adc': 'Adc',
+        'Jungle': 'Jungle',
+        'Top': 'Top',
+    };
+
+    const timesArray = Object.values(times);
 
     return (
         <section className="formulario">
@@ -10,6 +21,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa itens={timesArray} />
             </form>
         </section>
     )
