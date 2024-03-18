cdc128c489bfae57580cd1891e5b7523b4ae1cbb
MaiiZuko <maiaramzucco@gmail.com>
2024-03-18 16:25:58 -0300

    times

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,19 +1,50 @@
 import { useState } from 'react';
 import Banner from './componentes/banner';
 import Formulario from './componentes/formulario';
+import Time from './componentes/time';
 
 function App() {
 
-  const [campeao, setCampeao] = useState([])
+  const times = [
+    {
+      nome: 'Suporte',
+      corPrimaria: '#bfdbbf',
+      corSecundaria: '#abe7bd',
+    },
+    {
+      nome: 'Mid',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
+    },
+    {
+      nome: 'Adc',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8',
+    },
+    {
+      nome: 'Jungle',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5',
+    },
+    {
+      nome: 'Top',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9',
+    }
+  ]
+
+  const [campeoes, setCampeao] = useState([])
 
   const aoNovoCampeaoAdicionado = (campeao) => {
     console.log(campeao)
-
+    setCampeao([...campeoes, campeao])
   }
   return (
     <div className="App">
-      <Banner/>
-      <Formulario aoCampeaoCadastrado={campeao => aoNovoCampeaoAdicionado(campeao)}/>
+      <Banner />
+      <Formulario aoCampeaoCadastrado={campeao => aoNovoCampeaoAdicionado(campeao)} />
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria}
+        corSecundaria={time.corSecundaria} />)}
     </div>
   );
 }
---
+++ organo/src/componentes/time/index.js
@@ -0,0 +1,11 @@
+import './Time.css'
+
+const Time = (props) => {
+    return(
+        <selection className="time">
+            <h3>{props.nome}</h3>
+        </selection>
+    )
+}
+
+export default Time
